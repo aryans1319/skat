234dba55731358a40e6bfede596020833db4d676
Aryan <aryan4p130202@gmail.com>
2024-01-01 20:23:44 +0530

    feat: socket listeners implemented

--- apps/server/src/service/socket.ts
+++ apps/server/src/service/socket.ts
@@ -8,6 +8,20 @@ class SocketService {
         this._io = new Server();
     }
 
+    public socketListeners(){
+        const io = this.io;
+        console.log("Init the socket listeners");
+        
+        // when we connect to the socket
+        io.on("connect", (socket) => {
+            console.log("New socket connection", socket.id);
+            // when an event is emitted on the socket
+            socket.on("event: message", async({ message } : { message: String}) => {
+                console.log("New Message Received", message);
+            });
+        });
+    }
+
     get io(){
         return this._io;
     }
