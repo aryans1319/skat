cbe6a8f962df254966d36d0d5e67c072061fe9de
Aryan <aryan4p130202@gmail.com>
2024-01-01 20:13:07 +0530

    feat: socket server initialized and configured

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,17 @@
 import http from 'http'
+import SocketService from './service/socket';
 
 async function init() {
-    const httpServer = http.createServer();
+    const socketService = new SocketService();
     
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT ? process.env.PORT : 5000;
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT, () => 
+        console.log(`HTTP Server started at PORT ${PORT}`)
+    );
 }
+
+init();
